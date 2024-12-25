625f58365b6406e925261bb1161ad4862d1f7385
sagarDeyExavalu <sagar.dey@exavalu.com>
2024-12-25 13:22:16 +0530

    Initial commit and build the navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b px-5 h-14 items-center">
+      <Link href="/">Logo</Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-400 hover:text-zinc-800 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
